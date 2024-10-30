b6dd584b4ae197113a473b64689383310b7b8a7e
kennyrogers330 <kennyrogers330@gmail.com>
2024-10-30 23:17:47 +0200

    Added and adverts banner

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,45 +1,88 @@
-import React from 'react';
-import Header from '../Components/Header';
-import ServicesSection from '../Components/ServicesSection';
-import Recents from '../Components/Recents';
-import BottomNavBar from '../Components/BottomNavBar';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faQuestion, faUtensils, faPersonWalkingDashedLineArrowRight, faClipboardList, faCommentDots
-} from "@fortawesome/free-solid-svg-icons"
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
-
-
+import React from "react";
+import Header from "../Components/Header";
+import ServicesSection from "../Components/ServicesSection";
+import Recents from "../Components/Recents";
+import BottomNavBar from "../Components/BottomNavBar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faQuestion,
+  faUtensils,
+  faPersonWalkingDashedLineArrowRight,
+  faClipboardList,
+  faCommentDots,
+} from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import Carousel from "../Components/carousel/carousel";
 
 const Home = () => {
   const services = [
-    { name: 'Food Ordering',icon: <FontAwesomeIcon icon ={faUtensils} style={{ color: '#FBA4D3', }} size="xs"/>},
-    { name: 'Queuing', icon: <FontAwesomeIcon icon ={faPersonWalkingDashedLineArrowRight} style={{ color: '#FF4646' }}/>},
-    { name: 'Fill Survey', icon: <FontAwesomeIcon icon ={faClipboardList} style={{ color: '#59CAB3' }}/>},
-    { name: 'Feedback', icon: <FontAwesomeIcon icon ={faCommentDots}  style={{ color: '#FFBF00' }}/>},
-    { name: 'Buy Data', icon: <FontAwesomeIcon icon ={faUtensils}  style={{ color: '#824394' }}/>},
-    { name: 'Need Help', icon: <FontAwesomeIcon icon ={faQuestion} style={{ color: '#4285F4' }}/> },
-
+    {
+      name: "Food Ordering",
+      icon: (
+        <FontAwesomeIcon
+          icon={faUtensils}
+          style={{ color: "#FBA4D3" }}
+          size="xs"
+        />
+      ),
+    },
+    {
+      name: "Queuing",
+      icon: (
+        <FontAwesomeIcon
+          icon={faPersonWalkingDashedLineArrowRight}
+          style={{ color: "#FF4646" }}
+        />
+      ),
+    },
+    {
+      name: "Fill Survey",
+      icon: (
+        <FontAwesomeIcon icon={faClipboardList} style={{ color: "#59CAB3" }} />
+      ),
+    },
+    {
+      name: "Feedback",
+      icon: (
+        <FontAwesomeIcon icon={faCommentDots} style={{ color: "#FFBF00" }} />
+      ),
+    },
+    {
+      name: "Buy Data",
+      icon: <FontAwesomeIcon icon={faUtensils} style={{ color: "#824394" }} />,
+    },
+    {
+      name: "Need Help",
+      icon: <FontAwesomeIcon icon={faQuestion} style={{ color: "#4285F4" }} />,
+    },
   ];
 
   const recents = [
-    { icon: <FontAwesomeIcon icon ={faEnvelope}/>, label: 'Food Ordering' },
-    { icon: <FontAwesomeIcon icon ={faEnvelope}/>, label: 'Queuing' },
-    { icon: <FontAwesomeIcon icon ={faEnvelope}/>, label: 'Others' },
+    { icon: <FontAwesomeIcon icon={faEnvelope} />, label: "Food Ordering" },
+    { icon: <FontAwesomeIcon icon={faEnvelope} />, label: "Queuing" },
+    { icon: <FontAwesomeIcon icon={faEnvelope} />, label: "Others" },
   ];
 
   return (
     <div className="flex flex-col h-screen">
       <Header />
-      <main className="flex-grow bg-gray-100 p-6">
-        <div className="hidden lg:block bg-cover bg-center h-64 md:h-96" style={{ backgroundImage: 'url(/your-hero-image.jpg)' }}>
+      <main className="flex flex-col bg-gray-100 p-6 md:p-2">
+        {/* <div
+          className="lg:block bg-cover bg-center h-64 md:h-96 border border-black"
+          style={{ backgroundImage: "url(/your-hero-image.jpg)" }}
+        >
           <div className="flex items-center justify-center h-full bg-black bg-opacity-50">
-            <h1 className="text-white text-4xl md:text-6xl font-bold">Welcome to Our Digital Platform</h1>
+            <h1 className="text-white text-4xl md:text-6xl font-bold">
+              Welcome to Our Digital Platform
+            </h1>
           </div>
+        </div> */}
+        <div className="w-full lg:h-64 h-32 shadow-md">
+          <Carousel />
         </div>
         <div className="p-6">
           <Recents title="Recents." recents={recents} />
           <ServicesSection title="Our Services" services={services} />
-          
         </div>
       </main>
       <BottomNavBar />
--- frontend/src/Pages/Welcome_page.jsx
+++ frontend/src/Pages/Welcome_page.jsx
@@ -1,36 +1,36 @@
-import React, { useState } from 'react';
-import Login from './Login';
-import Footer from '../Components/Footer';
-import { Link } from 'react-router-dom';
+import React from "react";
+
+import Footer from "../Components/Footer";
+import { Link } from "react-router-dom";
 
 const Welcome = () => {
-
   const handleGetStarted = (e) => {
     e.preventDefault();
   };
 
   return (
     <>
-        <div className="min-h-screen flex flex-col justify-between bg-[#002D74]">
-          <div className="flex-grow flex flex-col items-center justify-center">
-            <h1 className="text-white text-lg  font-bold">Welcome to Company's Wi-Fi</h1>
-            <div>
-              <form onSubmit={handleGetStarted}>
-                <Link to='/login'>
+      <div className="min-h-screen flex flex-col justify-between bg-[#002D74]">
+        <div className="flex-grow flex flex-col items-center justify-center">
+          <h1 className="text-white text-lg  font-bold">
+            Welcome to Company's Wi-Fi
+          </h1>
+          <div>
+            <form onSubmit={handleGetStarted}>
+              <Link to="/login">
                 <button
-                  type="submit" 
+                  type="submit"
                   className="h-12 px-6 m-2 py-3 px-2 text-sm font-semibold tracking-wide rounded-full text-[#002D74] bg-white focus:outline-none"
                 >
                   Accept and Connect
                 </button>
-                </Link>
-              </form>
-            </div>
+              </Link>
+            </form>
           </div>
-          <Footer />
         </div>
-        <div className="login-container min-h-screen flex items-center justify-center">
-        </div>
+        <Footer />
+      </div>
+      <div className="login-container min-h-screen flex items-center justify-center"></div>
     </>
   );
 };
