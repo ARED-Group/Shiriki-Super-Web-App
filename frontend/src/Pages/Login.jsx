cb6c27b705f697c04a4334a1bc9e0e0196000d31
saudat28 <saudarsaleh@gmail.com>
2024-10-31 16:47:50 +0200

    Linked a verification route to the Login

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -82,7 +82,7 @@ const Login = () => {
               </label>
 
               <div className="!mt-8">
-               <Link to='/homepage'>
+               <Link to='/verification'>
                 <button 
                     type="submit" 
                     className={`h-12 px-6 m-2 py-3 px-2 text-sm font-semibold tracking-wide rounded-full text-white ${agreeToTerms ? 'bg-[#002D74] cursor-pointer' : 'bg-gray-400 cursor-not-allowed'}`}
