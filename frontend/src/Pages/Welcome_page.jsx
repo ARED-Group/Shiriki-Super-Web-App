7eb42e65de92fbcadff0516591237691ec27b795
kennyrogers330 <kennyrogers330@gmail.com>
2024-11-06 22:28:37 +0200

    commit to wifi feature

--- frontend/src/Pages/Welcome_page.jsx
+++ frontend/src/Pages/Welcome_page.jsx
@@ -1,36 +1,69 @@
 import React from "react";
-
-import Footer from "../Components/Footer";
 import { Link } from "react-router-dom";
+import { BiWifi } from "react-icons/bi";
+import logo from "./../assets/images/bkarena.png";
 
 const Welcome = () => {
-  const handleGetStarted = (e) => {
-    e.preventDefault();
-  };
-
   return (
     <>
-      <div className="min-h-screen flex flex-col justify-between bg-[#002D74]">
-        <div className="flex-grow flex flex-col items-center justify-center">
-          <h1 className="text-white text-lg  font-bold">
-            Welcome to Company's Wi-Fi
-          </h1>
-          <div>
-            <form onSubmit={handleGetStarted}>
-              <Link to="/login">
-                <button
-                  type="submit"
-                  className="h-12 px-6 m-2 py-3 px-2 text-sm font-semibold tracking-wide rounded-full text-[#002D74] bg-white focus:outline-none"
-                >
-                  Accept and Connect
+      <div className="h-screen flex items-start justify-center py-6 bg-[#002D74]">
+        <div className="flex flex-col w-96 h-full items-center justify-between">
+          <div className="flex flex-col">
+            <div>
+              <BiWifi size={150} className="text-slate-100" />
+            </div>
+          </div>
+
+          <div className="border border-[#002D74] rounded-md shadow-xl">
+            <img src={logo} alt="" className="w-36" />
+          </div>
+
+          <div className="w-full flex flex-col items-center space-y-4">
+            <div className="px-2">
+              <button
+                className="relative group overflow-hidden px-8 h-12 rounded-md bg-lime-500
+                      before:absolute 
+                      before:inset-0 
+                      before:bg-lime-600 
+                      before:scale-y-[0.1] 
+                      before:origin-bottom
+                      before:transition
+                      before:duration-300
+                      hover:before:scale-y-100
+                      "
+              >
+                <span className="relative uppercase text-base text-white">
+                  Connect to wifi
+                </span>
+              </button>
+            </div>
+            <div className="px-2 w-full">
+              <Link to="/homepage">
+                <button className="relative group overflow-hidden px-6 w-full h-12 rounded-full flex space-x-2 items-center justify-center bg-gradient-to-r from-blue-800 to-blue-500 hover:to-purple-600">
+                  <span className="relative text-sm text-white">Later</span>
+                  <div className="flex items-center -space-x-3 translate-x-3">
+                    <div className="w-2.5 h-[1.6px] rounded bg-white origin-left scale-x-0 transition duration-300 group-hover:scale-x-100"></div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-5 w-5 stroke-white -translate-x-2 transition duration-300 group-hover:translate-x-0"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M9 5l7 7-7 7"
+                      />
+                    </svg>
+                  </div>
                 </button>
               </Link>
-            </form>
+            </div>
           </div>
         </div>
-        <Footer />
       </div>
-      {/* <div className="login-container min-h-screen flex items-center justify-center"></div> */}
     </>
   );
 };
