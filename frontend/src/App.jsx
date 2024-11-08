d531e03882ec8c45884f24b9124c88e427808370
saudat28 <saudarsaleh@gmail.com>
2024-11-08 08:45:51 +0200

    Resolved merged conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,4 @@
-<<<<<<< HEAD
-=======
-// import React from 'react';
-// import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
-// import './App.css';
-// import { I18nextProvider } from 'react-i18next';
-// import './Components/i18n';
-// import AppRoutes from './Routes/routes';
-
-// function App() {
-//   return (
-//     <div className="App bg-[#002D74]">
-//       <BrowserRouter> {/* Wrap Routes inside BrowserRouter */}
-//         <AppRoutes /> {/* This renders the  main routes */}
-//       </BrowserRouter>
-//     </div>
-//   );
-// }
-
-// export default App;
->>>>>>> c5c4a76a4a4f3f7b4b392b7c185f29d1926a0102
-
+//App.jsx
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
